e5a9eae81d170d3e07e5917b483c27aaa053eea1
mdirshadalam6272 <134299659+mdirshadalam6272@users.noreply.github.com>
2023-05-22 20:00:57 +0530

    Add files via upload

---
+++ joke.js
@@ -0,0 +1,332 @@
+let jokes = 
+    [
+     {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Hey Girl,\nRoses are #ff0000,\nViolets are #0000ff,\nI use hex codes,\nBut I'd use RGB for you.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 41,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "पति ने कहा- राक्षस से शादी करके भी मैं इतना परेशान नहीं रहता जितना आज हूं, पत्नी ने यूं कर दी बोलती बंद",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 27,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Pun",
+            "type": "single",
+            "joke": "आओ 2 Min मौन रखे उन लड़कियों के लिए.. जिनकी शक्ल Nokia 1100 जैसी है और घमंड Iphone14 जैसा…. 😆🤣😋😉",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 236,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Dark",
+            "type": "single",
+            "joke": "शादी के दो दिन बाद दूल्हा उस ब्यूटी पार्लर में गया जहां से उसकी बीवी ने शादी के लिए मेकअप करवाया था उसने पार्लर वाली मैडम को आईफोन 7 गिफ्ट किया और थैंक्यू बोलकर चला                         आया मैडम ने खुशी खुशी आईफोन का डब्बा खोला तो उसमें नोकिया 1100 पड़ा था साथ में एक पर्ची भी थी जिस पर लिखा था मुझे भी ऐसा ही महसूस हुआ था😆🤣😋😉 ",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 138,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "भिखारी – ऐ भाई एक रुपया देदे, तिन दिन से भूखा हूँ।राहगीर – तिन दिन से भूखा है तो एक रुपए का क्या करेगा..?भिखारी – वजन तोलूंगा कितना घटा है..!!😂😜😅😂😂😜",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 3,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "एक लड़का एक सुंदर लड़की को बड़ी देर से घूर रहा था लड़की (गुस्से में): क्या देख रहे हो?लड़का (हड़बड़ी में): देख रहा हूँ कि अगर तुम मेरी अम्मा होती तो मैं भी सुन्दर होता..!",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 36,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "पत्नी:- आपको मेरी सुंदरता ज़्यादा अच्छी लगती है या मेरे संस्कार…?पति:- मुझे तो तेरी ये मज़ाक करने की आदत बहुत अच्छी लगती है।😜 😜 😜 😜 😜 😜 😜 😜",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 51,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "लड़का : पता है, बस में बैठते हुए मैं किसी लड़की को खड़ा नहीं देख सकतालड़की : तो फिर तुम क्या करते हो..?लड़का : मैं अपनी आँखें बंद कर लेता हूँ..!!!😂😜😅😂😂😜",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 123,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Pun",
+            "type": "single",
+            "joke": "टीचर: ‘हिम्मत ए मर्दा तो मदद ए खुदा’ का मतलब बताओ,?बच्चा: जो अपनी बीवी के सामने मर्द बनने कीकोशिश करता है,उसकी मदद फिर खुदा ही करसकता है…”😄👌😄👌",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 200,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Misc",
+            "type": "single",
+            "joke": "बबलू -तू स्कूल क्यों नही जातापप्पू- कई बार गया अंकल वो वापिस भगा देते है बबलू -क्यों ? पप्पू- कहते है भाग तेरा क्या काम लड़कियों के स्कूल में 😂😂😂.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 197,
+            "safe": true,
+            "lang": "en"
+        },
+      {
+            "category": "Pun",
+            "type": "single",
+            "joke": "संता की बीवी – सुनिए जी,रात नींद में आप मुझे गालियाँ दे रहे थे,संता – ओ नहीं सोणिये, ये तुम्हारा वहम है, बीवी – क्या वहम है? संता- यही कि मैं नींद में था. 😳😳😳😳😳😳😳   Santa's wife - Listen sir,You were abusing me in my sleep at night,Santa - Oh don't sleep,This is your illusionWife - What is the illusion?Santa - That's it I was sleeping.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 200,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Misc",
+            "type": "single",
+            "joke": "भिखारी – कुछ खाने को दे दे बेटा मैं बहुत लाचार हूँ…आदमी – देखने में तो हट्टे-कट्टे हो फिर लाचार किससे हो..?भिखारी – अपनी आदत से…😂😜😅😂😂😜",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "safe": true,
+            "id": 271,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Sanskrit Teacher –What is the meaning of‘तमसो मा ज्योर्तिगमय’🤔Student Said –“तू सो जा 👩 माँ, मैं ज्योति 👧 के घर जा रहा हुँ ।”😜😂😂",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "safe": true,
+            "id": 319,
+            "lang": "en"
+        },
+        {
+            "category": "Dark",
+            "type": "single",
+            "joke": "My ex had an accident. I told the paramedics the wrong blood type for her. She'll finally experience what rejection is really like.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 152,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Dark",
+            "type": "single",
+            "joke": "Doctor: \"I have some news about your baby.\"\nParents: \"Don't tell us the gender, we want to keep it a surprise.\"\nDoctor: \"Oh I get it, you're those type of people. Okay, well IT is not breathing.\"",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 138,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Pun",
+            "type": "single",
+            "joke": "Two fish in a tank. One turns to the other and says, \"Do you know how to drive this thing?\"",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 185,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Dark",
+            "type": "single",
+            "joke": "Me and my Jewish friend were eating lunch and I farted. He got mad so I said \"cmon man a little gas never killed anyone\".",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": true,
+                "sexist": false,
+                "explicit": true
+            },
+            "id": 103,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "ASCII silly question, get a silly ANSI.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 25,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Pun",
+            "type": "single",
+            "joke": "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 204,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "There are only 10 kinds of people in this world: those who know binary and those who don't.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 35,
+            "safe": true,
+            "lang": "en"
+        }
+    ]
+
+let bnext = document.getElementById('btn')
+let next = document.getElementById('para')
+
+bnext = addEventListener("click",()=>{
+let index = Math.floor(Math.random() * (jokes.length- 1))
+next.innerHTML = jokes[index].joke
+});
+
